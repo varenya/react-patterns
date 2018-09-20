40e480a64c468695437a983a027cdce90a7e6fa5
Varenya <varen90@gmail.com>
2018-09-20 23:17:59 +0530

    compound pattern

--- src/components/tab/Tab.js
+++ src/components/tab/Tab.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 import styled from "react-emotion";
 
-const TabHeadingContainer = styled("ul")`
+const TabHeadingContainerStyles = styled("ul")`
   display: flex;
   flex-flow: row wrap;
   margin: 0;
@@ -32,32 +32,74 @@ const TabContent = styled("div")`
   background: #fff;
 `;
 
+function TabContentWrapper(props) {
+  const { selectedTab } = props;
+  return props.children[selectedTab];
+}
+
+function TabHeadingContainer(props) {
+  const { activeIndex, children, handleClick } = props;
+  return (
+    <TabHeadingContainerStyles>
+      {React.Children.map(children, (child, index) => {
+        if (child.type === TabHeading)
+          return React.cloneElement(child, {
+            active: activeIndex === index,
+            className: activeIndex === index ? "active" : "",
+            onClick: () => handleClick(index)
+          });
+        else return null;
+      })}
+    </TabHeadingContainerStyles>
+  );
+}
+
 class Tabs extends React.Component {
   state = { selectedTab: 0 };
   handleClick = index => {
     this.setState({ selectedTab: index });
   };
   render() {
-    const { tabData } = this.props;
     const { selectedTab } = this.state;
+    const { children } = this.props;
     return (
       <Fragment>
-        <TabHeadingContainer>
-          {tabData.map(({ label }, index) => (
-            <TabHeading
-              key={index}
-              active={index === selectedTab}
-              className={index === selectedTab ? "active" : ""}
-              onClick={this.handleClick.bind(null, index)}
-            >
-              {label}
-            </TabHeading>
-          ))}
-        </TabHeadingContainer>
-        <TabContent>{tabData[selectedTab].description}</TabContent>
+        {React.Children.map(children, (child, index) => {
+          if (child.type === TabHeadingContainer) {
+            return React.cloneElement(child, {
+              handleClick: this.handleClick,
+              activeIndex: selectedTab
+            });
+          } else if (child.type === TabContentWrapper) {
+            return React.cloneElement(child, {
+              selectedTab
+            });
+          }
+        })}
       </Fragment>
     );
   }
 }
 
-export default Tabs;
+class TabsDefault extends React.Component {
+  render() {
+    const { tabData } = this.props;
+
+    return (
+      <Tabs>
+        <TabHeadingContainer>
+          {tabData.map(({ label }, index) => (
+            <TabHeading key={index}>{label}</TabHeading>
+          ))}
+        </TabHeadingContainer>
+        <TabContentWrapper>
+          {tabData.map(({ description }, index) => (
+            <TabContent key={index}>{description}</TabContent>
+          ))}
+        </TabContentWrapper>
+      </Tabs>
+    );
+  }
+}
+
+export default TabsDefault;
